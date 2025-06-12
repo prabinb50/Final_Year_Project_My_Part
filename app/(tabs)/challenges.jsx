ab8ee3613d5710a65aa414f656447fa172af916f
Prabin Joshi <joshiprabin17@gmail.com>
2025-06-12 16:10:00 +0545

    implement home screen

--- app/(tabs)/challenges.jsx
+++ app/(tabs)/challenges.jsx
@@ -1,6 +1,10 @@
 import { View, Text, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const Challenges = () => {
     // data for streak days
@@ -19,10 +23,9 @@ const Challenges = () => {
             <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false}>
                 {/* Streak Section */}
                 <View className="bg-white rounded-xl p-4 mt-4 shadow-lg">
-                    {/* First child */}
                     <Text className="font-bold text-base mb-3">Your Streak</Text>
 
-                    {/* Second Child - Streak Days */}
+                    {/* Streak Days */}
                     <View className="flex-row items-center justify-between">
                         {streakData.map((item, index) => (
                             <View key={index} className="items-center">
@@ -47,6 +50,63 @@ const Challenges = () => {
                                 </Text>
                             </View>
                         ))}
+                    </View>
+                </View>
+
+                {/* Today's Mission Section */}
+                <View className="p-2 mt-5">
+                    {/* Mission Header */}
+                    <View className="flex-row justify-between items-center mb-3">
+                        <Text className="text-base font-bold">Today's Mission</Text>
+
+                        {/* Timer */}
+                        <View className="flex-row items-center">
+                            <Ionicons name="timer-outline" size={18} color="#E2B100" />
+                            <Text className="ml-1 text-yellow-500 font-bold">03:25:55</Text>
+                        </View>
+                    </View>
+
+                    {/* Mission Card */}
+                    <View className="bg-yellow-100 rounded-xl p-4 shadow-lg">
+                        <View className="flex-row items-center gap-2">
+                            {/* Mission Icon */}
+                            <View className="bg-green-200 rounded-lg mr-3 w-12 h-12 items-center justify-center">
+                                <MaterialCommunityIcons name="recycle-variant" size={24} color="#00A653" />
+                            </View>
+
+                            {/* Mission Details */}
+                            <View className="">
+                                <Text className="font-semibold ">Plastic Bottle Blitz</Text>
+                                <Text className="text-sm text-gray-600 mb-2">Recycle 5 plastic bottles today</Text>
+
+                                {/* Progress Indicator */}
+                                <View className="mb-2 flex-row items-center justify-between">
+                                    {/* Progress Label */}
+                                    <Text className="font-semibold">2/5</Text>
+
+                                    {/* Progress Bar */}
+                                    <View className="w-70 h-1.5 bg-gray-200 rounded-full">
+                                        <View
+                                            className="h-full bg-green-500 rounded-full"
+                                            style={{ width: `${(2 / 5) * 100}%` }}
+                                        />
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+
+                        {/* Rewards*/}
+                        <View className="flex-row items-center justify-between mt-3">
+                            <View className="flex-row">
+                                <Entypo name="price-ribbon" size={18} color="#FFA600" />
+                                <Text className="font-semibold ml-1">Rewards:</Text>
+                            </View>
+
+                            <View className="flex-row">
+                                <FontAwesome5 name="coins" size={16} color="#FFA600" />
+                                <Text className="font-semibold ml-2">200</Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
             </ScrollView>
